89809558bc91c5c8addb0b972b101e19e5634f54
Zoriana Melnychuk <oriankaz17@gmail.com>
2025-07-08 14:56:15 +0200

    add task solution

---
+++ src/types/FilterField.ts
@@ -0,0 +1,5 @@
+export enum FilterField {
+  All = 'All',
+  Active = 'Active',
+  Completed = 'Completed',
+}
--- src/types/Todo.ts
+++ src/types/Todo.ts
@@ -1,6 +1,6 @@
 export interface Todo {
   id: number;
   title: string;
-  completed: boolean;
+  Completed: boolean;
   userId: number;
 }
